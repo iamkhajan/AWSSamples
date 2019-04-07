2ba6dd50419c95f0bbc96a27128f8c466ad2e320
Khajan Pandey <khajanchandra.pandey@mx.bosch.com>
2019-04-06 22:25:40 -0600

    updated with user agent

--- httpRequest.js
+++ httpRequest.js
@@ -1,32 +1,30 @@
-var http = require('http')
+var http = require('https')
 module.exports = {
-    httpGet: function(query, callback) {
-        return new Promise(((resolve, reject) => {
-          var options = {
-              host: 'numbersapi.com',
-              path: '/3',
-              method: 'GET',
-          };
-          console.log('function called')
-          //path: '/' + encodeURIComponent(query),
-          const request = http.request(options, (response) => {
-            response.setEncoding('utf8');
-            let returnData = '';
-      
-            response.on('data', (chunk) => {
-              returnData += chunk;
-            });
-      
-            response.on('end', () => {
-              console.log('function called with response '+ returnData)
-              resolve(returnData);
-            });
-      
-            response.on('error', (error) => {
-              reject(error);
-            });
-          });
-          request.end();
-        }));
-      }
+  httpGet: function(query, callback){
+    var options = {
+        host: 'api.github.com',
+        path:  query,
+        headers: {'User-Agent': "Mobile"},
+        method: 'GET',
+    };
+
+    var req = http.request(options, res => {
+        res.setEncoding('utf8');
+        var responseString = "";
+        
+        //accept incoming data asynchronously
+        res.on('data', chunk => {
+            responseString = responseString + chunk;
+        });
+        
+        //return the data when streaming is complete
+        res.on('end', () => {
+            var data = JSON.parse(responseString);
+            console.log('callback function end '+data);
+            callback(data);
+        });
+
+    });
+    req.end();
+}
 }
